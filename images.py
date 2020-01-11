3bd65a2402f0a08766dac8e4dac8cc8c58f406dd
HeresJohnny5 <ericksonjohn01@hotmail.com>
2020-01-11 08:02:39 -0500

    Delete images file. Add comment directions for converting jpg to png.

--- images.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image, ImageFilter
-im = Image.open('./astro.jpg')
-
-im.thumbnail((300, 300))
-im.save('small.png', 'png')
---
+++ jpg_to_png_converter.py
@@ -0,0 +1,2 @@
+# run file providing 2 arguments: file to open and new file to save
+# when opening the file provided from the first argument, loop through the images converting them from jpg to png before saving them to the newly created file provided by the second argument
