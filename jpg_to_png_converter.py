818559675b3c35791cb9f7a0fcfb16034f4a6aa0
HeresJohnny5 <ericksonjohn01@hotmail.com>
2020-01-11 10:36:35 -0500

    Refactor code using functions.

--- jpg_to_png_converter.py
+++ jpg_to_png_converter.py
@@ -5,45 +5,60 @@ import argparse
 import os
 from PIL import Image
 
-# CREATE FUNCTION
-parser = argparse.ArgumentParser(
-    description='Open a file, convert images from JPG format to PNG and save into a new file!')
 
-parser.add_argument('--o', default='./pokedex',
-                    help='This is the directory to be opened which contains jpg file format images to be converted to png.')
-parser.add_argument('--n', default='./converted/',
-                    help='This is the directory to be saved which contains images that have already been converted from jpg file format images to png.')
+def user_input():
+    parser = argparse.ArgumentParser(
+        description='Open a file, convert images from JPG format to PNG and save into a new file!')
 
-args = parser.parse_args()
-file_open = args.o
-new_directory = args.n
-# print('file_open: {}'.format(file_open))
-# print('new_file: {}'.format(new_file))
+    parser.add_argument('--o', default='./pokedex',
+                        help='This is the directory to be opened which contains jpg file format images to be converted to png.')
+    parser.add_argument('--n', default='./converted/',
+                        help='This is the directory to be saved which contains images that have already been converted from jpg file format images to png.')
 
-path = './pokedex/'
+    args = parser.parse_args()
+    directory_open = args.o
+    new_directory = args.n
 
-# CREATE FUNCTION
-images = []
-# r=root, d=directories, f = files
-for r, d, f in os.walk(file_open):
-    for image in f:
-        if '.jpg' in image:
-            images.append(image)
+    return {'old_dir': directory_open, 'new_dir': new_directory}
 
-# CREATE FUNCTION
-converted_images = []
-for image in images:
-    updated_image = image.replace('jpg', 'png')
-    converted_images.append(updated_image)
 
-# CREATE FUNCTION
-if not os.path.exists(new_directory):
-    os.makedirs(new_directory)
+def pulled_jpg_images(old_dir):
+    images = []
 
-# CREATE FUNCTION
-# complete_path = os.path.join('./converted', converted_images[0])
-# file1 = open(complete_path, 'w')
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(old_dir):
+        for image in f:
+            if '.jpg' in image:
+                images.append(image)
 
-for image in converted_images:
-    complete_path = os.path.join(new_directory, image)
-    im = open(complete_path, 'w+')
+    print(images)
+    return images
+
+
+def convert_jpg_to_png(images):
+    converted_images = []
+
+    for image in images:
+        updated_image = image.replace('jpg', 'png')
+        converted_images.append(updated_image)
+
+    print(converted_images)
+    return converted_images
+
+
+def create_directory():
+    if not os.path.exists(directories['new_dir']):
+        os.makedirs(directories['new_dir'])
+
+
+def save_png_files_to_new_dir(png_images):
+    for image in png_images:
+        complete_path = os.path.join(directories['new_dir'], image)
+        im = open(complete_path, 'w+')
+
+
+directories = user_input()
+jpg_images = pulled_jpg_images(directories['old_dir'])
+png_images = convert_jpg_to_png(jpg_images)
+create_directory()
+save_png_files_to_new_dir(png_images)
